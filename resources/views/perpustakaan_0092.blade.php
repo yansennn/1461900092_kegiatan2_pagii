d3dd7502278ebfaf977bf785543534aa42cca180
yansennn <yanuarius.yansen02@gmail.com>
2021-05-29 14:32:28 +0700

    kegiatan2

---
+++ resources/views/perpustakaan_0092.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width,
+    initial-scale=1">
+    <title>Data Perpustakaan</title>
+    <style>
+        table {
+        border-collapse: collapse;
+        border-spacing: 0;
+        width: 100%;
+        border: 1px solid #ddd;
+        }
+        thead {
+        background-color: #f2f2f2;
+        }
+        th, td {
+        text-align: left;
+        padding: 8px;
+        }
+        tr:nth-child(even){background-color: #f2f2f2}
+        .tambah{
+        padding: 8px 16px ;
+        text-decoration: none;
+        }
+    </style>
+</head>
+<body>
+    <div style="overflow-x:auto;">
+    <table>
+        <thead>
+            <tr>
+                <th>judul buku</th>
+                <th>jumlah buku</th>
+                <th>nama kategori</th>
+                <th>id kategori</th>
+            
+            </tr>
+        </thead>
+        <tbody>
+            <?php $no=1; ?>
+            @foreach ($perpustakaan_0092 as $prs)
+            <tr>
+                <td>{{$prs->judul_buku}}</td>
+                <td>{{$prs->jumlah_buku}}</td>
+                <td>{{$prs->nama_kategori}}</td>
+                
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PerpustakaanController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('perpustakaan_0092', PerpustakaanController::class);
